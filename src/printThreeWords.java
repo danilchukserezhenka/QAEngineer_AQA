e05a9dd68f395ce0744ec814e164d1b98ba38086
Данильчук Сергей <danilchuk.serezhenka@gmail.com>
2024-07-18 23:56:25 +0700

    1

---
+++ src/printThreeWords.java
@@ -0,0 +1,8 @@
+public class printThreeWords {
+    public static void main(String[] args) {
+        String s1 = "Orange\n";
+        String s2 = "Banana\n";
+        String s3 = "Apple";
+        System.out.println(s1 + s2 + s3);
+    }
+}
