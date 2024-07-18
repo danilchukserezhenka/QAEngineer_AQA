a508dbf0e8db17f57a89f59a99ded241435483ea
Данильчук Сергей <danilchuk.serezhenka@gmail.com>
2024-07-19 00:52:59 +0700

    4

---
+++ src/compareNumbers.java
@@ -0,0 +1,11 @@
+public class compareNumbers {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 3;
+        if (a >= b) {
+            System.out.print("a >= b");
+        } else {
+            System.out.print("a < b");
+        }
+    }
+}
