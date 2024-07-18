63d33f44f6954e298e58478669c49fc491baa6c2
Данильчук Сергей <danilchuk.serezhenka@gmail.com>
2024-07-19 00:42:46 +0700

    3

---
+++ src/printColor.java
@@ -0,0 +1,14 @@
+public class printColor {
+    public static void main(String[] args) {
+        int value = 34;
+        if ( value <= 0) {
+            System.out.print("Красный");
+        }
+        if ( 0 < value && value <= 100 ) {
+            System.out.print("Желтый");
+        }
+        if ( value > 100 ) {
+            System.out.print("Зеленый");
+        }
+    }
+}
