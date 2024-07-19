eb62bca45b6948eccaa356392d9514960324978f
Данильчук Сергей <danilchuk.serezhenka@gmail.com>
2024-07-19 13:57:37 +0700

    6

---
+++ src/zadanie7.java
@@ -0,0 +1,11 @@
+public class zadanie7 {
+    public static void main(String[] args) {
+        System.out.println(summa(5));
+        System.out.println(summa(-50));
+        System.out.println(summa(0));
+
+    }
+    public static boolean summa( int a ){
+        return a <0;
+    }
+}
