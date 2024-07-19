bb7d86e5f8041dba84187243573aec17b075447a
Данильчук Сергей <danilchuk.serezhenka@gmail.com>
2024-07-19 08:00:22 +0700

    5

--- src/zadanie5.java
+++ src/zadanie5.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
+
 public class zadanie5 {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        System.out.println("Enter two numbers (x,y)");
-        System.out.print("Number x: ");
-        int x = console.nextInt();
-        System.out.print("Number y: ");
-        int y = console.nextInt();
-
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        int b;
+        System.out.print("Введите число а");
+        a = scanner.nextDouble();
+        System.out.print("Введите число b");
+        b = scanner.nextDouble();
+        int s = a + b;
+        System.out.print(s);
+        scanner.close();
+        }
     }
-}
+
