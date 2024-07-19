9f4912ba5d407fc2a9e228a740c956002c32b563
Данильчук Сергей <danilchuk.serezhenka@gmail.com>
2024-07-19 13:12:03 +0700

    5

--- src/zadanie5.java
+++ src/zadanie5.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class zadanie5 {
     public static void main(String[] args) {
-        System.out.println(maxFinder(30, 6));
+        System.out.println(summa(5, 6));
     }
-    public static boolean maxFinder( int a, int b ){
-        return ( a + b ) >= 10 && ( a + b ) <= 20;
+    public static boolean summa( int a, int b ){
+        return  a + b  >= 10 &&  a + b  <= 20;
     }
 }
 
