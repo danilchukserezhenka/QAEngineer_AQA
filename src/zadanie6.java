7777e5ac961a3c12e6337ac139e58f1ba5fa118c
Данильчук Сергей <danilchuk.serezhenka@gmail.com>
2024-07-19 13:36:46 +0700

    6

--- src/zadanie6.java
+++ src/zadanie6.java
@@ -1,6 +1,16 @@
 public class zadanie6 {
     public static void main(String[] args) {
-
+        chislo(4);
+        chislo(-4);
+        chislo(0);
+    }
+    public static void chislo(int a) {
+        System.out.print("Число " + a + ": ");
+        if (a >= 0) {
+            System.out.println("положительное");
+        } else {
+            System.out.println("отрицательное");
+        }
     }
 
 
