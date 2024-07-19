b1213ab262ab7004b9ae60b5380dbedca9c56a18
Данильчук Сергей <danilchuk.serezhenka@gmail.com>
2024-07-19 13:22:52 +0700

    5

--- src/zadanie5.java
+++ src/zadanie5.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class zadanie5 {
     public static void main(String[] args) {
         System.out.println(summa(5, 6));
+        System.out.println(summa(50, 6));
+        System.out.println(summa(4, 6));
+
     }
     public static boolean summa( int a, int b ){
         return  a + b  >= 10 &&  a + b  <= 20;
