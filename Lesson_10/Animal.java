fee96d7724e6645e962e7e106f45a9719b621d47
Данильчук Сергей <danilchuk.serezhenka@gmail.com>
2024-08-18 19:41:58 +0700

    Cotrudnik

--- Lesson_10/Animal.java
+++ Lesson_10/Animal.java
@@ -1,9 +1,17 @@
 /**
 Класс животные
  **/
-public class Animals {
-    public String tip;
-    public String name;
-    public int zarplata;
-    public int vozrost;
+public class Animal {
+    String name;
+    public Animal() {
+    }
+    public Animal(String name) {
+        this.name = name;
+    }
+    public void animalinfo() {
+        System.out.println("Животное: " + name);
+    }
+    public void jump() {
+        System.out.println("Животное прыгнуло");
+    }
 }
--- Lesson_10/Kot.java
+++ Lesson_10/Kot.java
@@ -1,1 +1,33 @@
-
+public class Animal {
+    String name;
+    public Animal() {
+    }
+    public Animal(String name) {
+        this.name = name;
+    }
+    public void animalinfo() {
+        System.out.println("Животное: " + name);
+    }
+    public void jump() {
+        System.out.println("Животное прыгнуло");
+    }
+}
+public class Kot extends Animal {
+    String color;
+    public Kot(String name, String  color) {
+        this.name = name;
+        this.color = color;
+    }
+    public void catInfo() {
+        System.out.println("Кот имя: " + name + "цвет: " + color);
+    }
+}
+public class AnimalApp {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Дружок");
+        Cat cat = new Cat("Барсик", "Белый");
+        animal.animalInfo();
+        cat.animalInfo();
+        cat.catInfo();
+    }
+}
