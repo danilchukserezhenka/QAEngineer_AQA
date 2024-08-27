2328469d1dc29801f2d0fb0df6281cb198d1af13
Данильчук Сергей <danilchuk.serezhenka@gmail.com>
2024-08-27 17:20:25 +0700

    Подготовка

--- Lesson_5/vivodCotrudnik.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class vivodCotrudnik {
-    public static void main(String[] args) {
-        Cotrudnik Cotrudnik1 = new Cotrudnik( "1", "2", "3", "4", 5, 6);
-        Cotrudnik1.print();
-    }
-}
-
